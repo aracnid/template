c585f7c30e3307596384b78749029ee0c833d540
Jason Romano <aracnid@gmail.com>
2019-05-31 08:48:41 -0400

    initialize basic structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Template',
+    version='0.1dev',
+    packages=['template',],
+    license='',
+    long_description=open('README.md').read(),
+)
